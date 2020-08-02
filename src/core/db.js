30286f6c2848c013da706e5a04a283a975c80c9d
Vadym Stoilovskyi <vadym.stoilovskyi@gmail.com>
2020-08-02 15:06:50 +0300

    Unittests added for Users api

--- src/core/db.js
+++ src/core/db.js
@@ -2,42 +2,20 @@ const mongoose = require("mongoose");
 
 
 const connect = () => {
-    return new Promise((resolve, reject) => {
-
-        if (process.env.IS_TEST === 'test'){
-        const Mockgoose = require("mockgoose").Mockgoose;
-        const mockgoose = new Mockgoose(mongoose);
-
-        mockgoose.prepareStorage()
-            .then(() => {
-                mongoose.connect(process.env.DB_URI,
-                    {
-                        useUnifiedTopology: true,
-                        useNewUrlParser: true
-                    })
-                    .then((res, err) => {
-                        if (err) {
-                            return reject(err)
-                        }
-                        console.info('Connection to DB established');
-                        resolve()
-                    })
+    return new Promise(async (resolve, reject) => {
+        mongoose.connect(
+            process.env.DB_URI,
+            {
+                useUnifiedTopology: true,
+                useNewUrlParser: true
             })
-        } else {
-            mongoose.connect(
-                process.env.DB_URI,
-                {
-                    useUnifiedTopology: true,
-                    useNewUrlParser: true
-                })
-                .then((res, err) => {
-                    if (err) {
-                        return reject(err)
-                    }
-                    console.info('Connection to DB established');
-                    resolve()
-                })
-        }
+            .then((res, err) => {
+                if (err) {
+                    return reject(err)
+                }
+                console.info('Connection to DB established');
+                resolve()
+            })
     })
 }
 
--- tests/api/user/createUser.js
+++ tests/api/user/createUser.js
@@ -1,42 +1,204 @@
-process.env.IS_TEST = 'test';
-
 const expect = require('chai').expect;
 const request = require('supertest');
 
-const connect = require("../../../src/core/db")
-const app = require("../../../src/app")
+const app = require("../../../src/app");
 
-describe("Create user POST /user", () => {
-    before((done) => {
-        connect()
-            .then(() => done())
-            .catch((err) => console.log(err));
-    })
+const mongoose = require("mongoose");
+const { MongoMemoryServer } = require("mongodb-memory-server");
+let mongoServer;
+
+describe("Create user Positive POST /user", () => {
+    beforeEach((done) => {
+        mongoServer = new MongoMemoryServer();
+        mongoServer.getUri()
+            .then((mongoUri) => {
+                mongoose.connect(mongoUri, {useUnifiedTopology: true, useNewUrlParser: true})
+                done();
+            });
+    });
+    afterEach(async () => {
+        await mongoose.disconnect();
+        await mongoServer.stop();
+
+    });
 
     it("Valid creation of user", (done) => {
         request(app).post("/user")
             .send({
-                "firstName": "firstName",
-                "lastName": "lastName",
+                "firstName": "Test name",
+                "lastName": "Test Last Name",
                 "password": "123456",
                 "email": "test@gmail.com"
             })
-            .then((res) =>{
-                const body = res.body;
-                // expect(body).to.contain.property("firstName")
+            .then((res) => {
+                const user = res.body.user;
+                expect(user.firstName).to.equals("Test name")
+                expect(user.lastName).to.equals("Test Last Name")
+                expect(user).to.contain.property("password")
+                expect(user.email).to.equals("test@gmail.com")
+                expect(user).to.contain.property("_id")
                 done();
             })
             .catch((err) => done(err));
     });
 
-    it('OK, getting notes has no notes', (done) => {
-        request(app).get('/notes')
+    it("Valid creation of user min values", (done) => {
+        request(app).post("/user")
+            .send({
+                "firstName": "Te",
+                "lastName": "La",
+                "password": "123456",
+                "email": "qq@qa.ua"
+            })
             .then((res) => {
-                const body = res.body;
-                expect(body.length).to.equal(0);
+                const user = res.body.user;
+                expect(user.firstName).to.equals("Te")
+                expect(user.lastName).to.equals("La")
+                expect(user).to.contain.property("password")
+                expect(user.email).to.equals("qq@qa.ua")
+                expect(user).to.contain.property("_id")
                 done();
             })
             .catch((err) => done(err));
     });
+
+    it("Valid creation of user max values", (done) => {
+        request(app).post("/user")
+            .send({
+                "firstName": "max symbols first na",
+                "lastName": "max symbols last nam",
+                "password": "12345678901234567890123456789012345678901234567890",
+                "email": "qqqqqqqqqqqqqqqqqqqqqqqq@qaqqqqqqqqqqqqqqqqqqqqqqqqqqqq.ua"
+            })
+            .then((res) => {
+                const user = res.body.user;
+                expect(user.firstName).to.equals("max symbols first na")
+                expect(user.lastName).to.equals("max symbols last nam")
+                expect(user).to.contain.property("password")
+                expect(user.email).to.equals("qqqqqqqqqqqqqqqqqqqqqqqq@qaqqqqqqqqqqqqqqqqqqqqqqqqqqqq.ua")
+                expect(user).to.contain.property("_id")
+                done();
+            })
+            .catch((err) => done(err));
+    });
+})
+
+
+describe("Create user Negative POST /user", () => {
+    before((done) => {
+        mongoServer = new MongoMemoryServer();
+        mongoServer.getUri()
+            .then((mongoUri) => {
+                mongoose.connect(mongoUri, { useUnifiedTopology: true,  useNewUrlParser: true })
+                done();
+            });
+    })
+    after(async () => {
+        await mongoose.disconnect();
+        await mongoServer.stop();
+    });
+
+    it('errors due to required first name', (done) => {
+        request(app).post('/user')
+            .send({
+                "firstName": "",
+                "lastName": "Test Last Name",
+                "password": "123456",
+                "email": "test@gmail.com"
+            })
+            .then((res) => {
+                const body = res.body;
+                expect(res.status).to.equals(400);
+                expect(body.error).to.equals("\"firstName\" is not allowed to be empty");
+                done();
+            })
+            .catch((err) => done(err));
+    });
+
+    it('errors due to short first name', (done) => {
+        request(app).post('/user')
+            .send({
+                "firstName": "q",
+                "lastName": "Test Last Name",
+                "password": "123456",
+                "email": "test@gmail.com"
+            })
+            .then((res) => {
+                const body = res.body;
+                expect(res.status).to.equals(400);
+                expect(body.error).to.equals("\"firstName\" length must be at least 2 characters long");
+                done();
+            })
+            .catch((err) => done(err));
+    });
+
+    it('errors due to long first name', (done) => {
+        request(app).post('/user')
+            .send({
+                "firstName": "max symbols first nam",
+                "lastName": "Test Last Name",
+                "password": "123456",
+                "email": "test@gmail.com"
+            })
+            .then((res) => {
+                const body = res.body;
+                expect(res.status).to.equals(400);
+                expect(body.error).to.equals("\"firstName\" length must be less than or equal to 20 characters long");
+                done();
+            })
+            .catch((err) => done(err));
+    });
+
+    it('errors due to required last name', (done) => {
+        request(app).post('/user')
+            .send({
+                "firstName": "fname",
+                "lastName": "",
+                "password": "123456",
+                "email": "test@gmail.com"
+            })
+            .then((res) => {
+                const body = res.body;
+                expect(res.status).to.equals(400);
+                expect(body.error).to.equals("\"lastName\" is not allowed to be empty");
+                done();
+            })
+            .catch((err) => done(err));
+    });
+
+    it('errors due to short last name', (done) => {
+        request(app).post('/user')
+            .send({
+                "firstName": "fname",
+                "lastName": "q",
+                "password": "123456",
+                "email": "test@gmail.com"
+            })
+            .then((res) => {
+                const body = res.body;
+                expect(res.status).to.equals(400);
+                expect(body.error).to.equals("\"lastName\" length must be at least 2 characters long");
+                done();
+            })
+            .catch((err) => done(err));
+    });
+
+    it('errors due to long last name', (done) => {
+        request(app).post('/user')
+            .send({
+                "firstName": "fname",
+                "lastName": "max symbols last name",
+                "password": "123456",
+                "email": "test@gmail.com"
+            })
+            .then((res) => {
+                const body = res.body;
+                expect(res.status).to.equals(400);
+                expect(body.error).to.equals("\"lastName\" length must be less than or equal to 20 characters long");
+                done();
+            })
+            .catch((err) => done(err));
+    });
+
 })
 
