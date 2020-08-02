9e4f4957c3c50463a7683b7dda48257e922a39a5
Vadym Stoilovskyi <vadym.stoilovskyi@gmail.com>
2020-08-02 22:13:13 +0300

    Routs fix

--- src/routes/reminderRouter.js
+++ src/routes/reminderRouter.js
@@ -4,12 +4,11 @@ const verifyToken = require("../middlewares/verifyToken");
 const reminderRouter = (app) => {
     const Reminder = new ReminderController();
 
-    app.put("/reminder/:_id/", verifyToken,  Reminder.edit);
-
     app.patch("/reminder/invite/:_id/accept", verifyToken,  Reminder.acceptReminder);
     app.patch("/reminder/invite/:_id/decline", verifyToken,  Reminder.declineReminder);
     app.post("/reminder/invite", verifyToken,  Reminder.invite);
     app.get("/reminder/invite", verifyToken,  Reminder.getInvitedReminders);
+    app.put("/reminder/:_id/", verifyToken,  Reminder.edit);
     app.post("/reminder", verifyToken,  Reminder.create);
     app.get("/reminder", verifyToken,  Reminder.get);
     app.delete("/reminder", verifyToken,  Reminder.delete);
